5c694999b5410cab97a25c7ca5fe368b59f766ee
flyplugins <flyplugins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-07 01:11:57 +0000

    Update 1.1
    git-svn-id: https://plugins.svn.wordpress.org/exchange-membership-addon-for-wp-courseware/trunk@1084229 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-courseware-exchange.php
+++ wp-courseware-exchange.php
@@ -1,27 +1,13 @@
 <?php
 /*
  * Plugin Name: WP Courseware - Exchange Membership Add On
- * Version: 1.0
+ * Version: 1.1
  * Plugin URI: http://flyplugins.com
  * Description: The official extension for <strong>WP Courseware</strong> to add integration for the <strong>Exchange Membership plugin by iThemes</strong> for WordPress.
  * Author: Fly Plugins
  * Author URI: http://flyplugins.com
  */
-/*
- Copyright 2014 Fly Plugins - Lighthouse Media, LLC
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
 
 
 // Main parent class
